98d017f128c955b2c80b0dde1e6ad2c86b4a7d6c
CrisL1990 <lucchetta.cris@gmail.com>
2022-02-03 17:23:58 +0100

    Esercizio 1 corretto

--- Snack 1/main.js
+++ Snack 1/main.js
@@ -13,7 +13,7 @@ let bicicletta = [
 
     {
         nome : "bici3",
-        peso : 8
+        peso : 2
     },
 
     {
@@ -25,11 +25,15 @@ let bicicletta = [
 let biciLeggera = bicicletta[0];
 
 for (let i = 0; i < bicicletta.length; i++){
+
+    if(bicicletta[i].peso < biciLeggera.peso){
+        biciLeggera = bicicletta[i];
+    }
    
 }
 
 
-console.log(biciLeggera);
+console.log(`La bici più leggera è ${biciLeggera.nome} e il suo peso è ${biciLeggera.peso} kg`);
 
 
 
